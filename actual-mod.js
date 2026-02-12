9a056e2d763d485b8f17361889736ca37e812cda
yardendaskall-arch <yardendaskall@gmail.com>
2026-02-12 22:28:43 +0200

    Update actual-mod.js

--- actual-mod.js
+++ actual-mod.js
@@ -1,30 +1,12 @@
+console.log("MOD FILE LOADED");
+
 runAfterLoad(() => {
+    console.log("RUN AFTER LOAD WORKED");
 
-elements.code_element = {
-    color: "#00ffcc",
-    behavior: behaviors.WALL,
-    category: "special",
-    state: "solid",
-
-    tick(pixel) {
-        if (!pixel.code) return;
-        try {
-            new Function("pixel", pixel.code)(pixel);
-        } catch(e) {
-            console.log(e);
-        }
-    },
-
-    onSelect() {
-        elements.code_element.customCode =
-            prompt("Enter JS code:", "pixel.temp+=1") || "";
-    },
-
-    onPlace(pixel) {
-        pixel.code = elements.code_element.customCode || "";
-    }
-};
-
-console.log("Code element mod loaded");
-
+    elements.code_element = {
+        color: "#00ffcc",
+        behavior: behaviors.WALL,
+        category: "special",
+        state: "solid"
+    };
 });
